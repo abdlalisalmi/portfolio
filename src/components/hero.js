1cb9e573451cea3da23aeaf9443a544636eb4695
abdlalisalmi <abdlalisalmi@gmail.com>
2024-11-19 12:57:56 +0100

    Updated -- finished the hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,18 +4,23 @@ import { BackgroundLines } from "@/components/ui/backgroundLines";
 import { motion } from "framer-motion";
 import { ArrowDown2 } from "iconsax-react";
 import { FaGithub, FaLinkedinIn, FaStackOverflow } from "react-icons/fa";
+import { TextGenerateEffect } from "./ui/textGenerateEffect";
 
 export function Hero() {
   return (
     <main className="min-h-screen w-full flex flex-col justify-center items-center relative w-full h-full">
       <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 min-h-screen">
         <section className="z-10 flex flex-col items-center justify-center">
-          <Image
-            src="/images/profile.jpg"
-            alt="Profile"
-            width={200}
-            height={200}
-            className="rounded-full shadow-lg mx-auto mb-4"
+          <motion.img
+            src="/images/profile.jpg" // Replace with your image URL
+            alt="Fade In"
+            className="w-72 h-auto rounded-full shadow-lg mx-auto mb-4"
+            initial={{ opacity: 0 }} // Start fully transparent
+            animate={{ opacity: 1 }} // Fade to full opacity
+            transition={{
+              duration: 1.8, // Duration of fade-in
+              ease: "easeInOut", // Smooth transition
+            }}
           />
           <p className="p-2 text-xs bg-zinc-900 rounded-xl text-zinc-50 borde-2 border-zinc-100 hover:bg-zinc-800 transition duration-300 mb-4 font-mono">
             abdlalisalmi
@@ -25,7 +30,14 @@ export function Hero() {
             </span>
           </p>
           <h1 className="text-4xl text-zinc-300 font-bold text-center mb-4 font-mono">
-            Hi, I'm Abdelaali ES SALMI.
+            <TextGenerateEffect
+              className={
+                "text-4xl text-zinc-300 font-bold text-center font-mono"
+              }
+              words={"Hi, I'm Abdelaali ES SALMI."}
+              duration={1}
+            />
+            {/* Hi, I'm Abdelaali ES SALMI. */}
           </h1>
           <p className="text-md text-center text-zinc-400/90">
             I'm a full-stack web developer and software engineer with a passion
